2ef89d2eb24e23f65bb83d4a63e0a9d30e268209
Gi@da <giadavolpi8@gmail.com>
2019-12-16 15:16:01 +0100

    search ok

--- main.js
+++ main.js
@@ -1,25 +1,57 @@
-// mio API key => 157ee715886d595bb09f199045df67b5
-// esempio di API request => https://api.themoviedb.org/3/movie/550?api_key=157ee715886d595bb09f199045df67b5
 $(document).ready (function () {
 
-    $.ajax ({
-        // "url": baseURL_AP + myAPIKey,
-        "url": "https://api.themoviedb.org/3/search/movie",
-        "data": {
-            "api_key": "157ee715886d595bb09f199045df67b5",
-            "query": "ritorno+al+futuro"
-        },
-        "method": "GET",
-        "success": function (data) {
-            var film = data.results;
-            for (var i = 0; i < film.length; i++) {
-                var titolo = film[i].title
-                console.log(titolo);
-            }
-            },
-        "error": function () {
-            alert ("Error");
+    var urlBaseAPI = "https://api.themoviedb.org/3/"
+
+    // avvio la ricerca premendo il tasto ENTER della tastiera
+    $ ("#input-search").keypress (function (event) {
+        if (event.which == 13) {
+            avviaRicerca();
         }
     });
+
+    // avvio la ricerca cliccando sulla lente d'ingrandimento
+    $ ("#button-search").click (function () {
+        avviaRicerca();
+    });
+
+
+
+    function avviaRicerca () {
+        // prendo il contenuto testuale dell'input
+        var testoRicerca = $ ("#input-search").val ();
+        console.log(testoRicerca);
+
+        // chiamo ajax per confrontare il testo ricercato con i titolo dei film
+        $.ajax ({
+            "url": urlBaseAPI + "search/movie",
+            "data": {
+                // i primi due parametri sono obbligatori
+                "api_key": "157ee715886d595bb09f199045df67b5",
+                "query": testoRicerca,
+                "language": "it-IT"
+            },
+            "method": "GET",
+            "success": function (data) {
+                var film = data.results;
+                console.log(film);
+                for (var i = 0; i < film.length; i++) {
+                    var titolo = film[i].title;
+                    var titoloOriginale = film[i].original_title;
+                    var linguaOriginale = film[i].original_language;
+                    var voto = film[i].vote_average;
+                    console.log(titolo);
+                    console.log(titoloOriginale);
+                    console.log(linguaOriginale);
+                    console.log(voto);
+                    }
+                },
+            "error": function () {
+                alert ("Error");
+            }
+        });
+    }
+
+
+
 
 });
